31cfe9c43e86b2ce6e6f423a3b5f4f411269f8bc
jamesncox <jamesncox@yahoo.com>
2021-03-12 16:34:02 -0600

    Update pages/index.js title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Create Next App</title>
+                <title>Code Snippets</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
